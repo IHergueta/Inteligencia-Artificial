670d60198a37a0e93803cd4cf14a9fc019b79573
IHergueta <59694418+IHergueta@users.noreply.github.com>
2020-12-16 22:45:01 +0100

    Add files via upload
    Este ejercicio reconoce tu mensaje por voz y te lo repite aplicando las librerias pyttxs3, speech_recognition.

---
+++ Jarvis.py
@@ -0,0 +1,35 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install SpeechRecognition
+from playsound import playsound
+#pip install pipwin
+#pipwin install PyAudio
+#pip install playsound
+
+engine= pyttsx3.init()
+
+def speak(audio):
+
+    voices = engine.getProperty('voices') #cambiar voz
+    engine.setProperty('voice', voices[1].id) # 0 hombre , 1 mujer
+    engine.say(audio)
+    engine.runAndWait()
+
+def escuchar():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Eschuchando...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Reconociendo..")
+        query = r.recognize_google(audio, language='en-in')
+        print(query)
+
+
+    except Exception as e:
+        print(e)
+        speak("Repite")
+
+    return speak(query)
+escuchar()
